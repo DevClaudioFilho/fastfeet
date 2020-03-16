86db0dd303b80adbb5b445185c022458eeafc54a
Claudio Filho <devclaudiofilho@gmail.com>
2020-03-16 14:59:26 -0300

    react native

---
+++ mobile/src/index.js
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View,Text} from 'react-native';
+import Routes from "./routes";
+
+import './config/ReactotronConfig'
+
+export default function App(){
+  return (
+    <>
+      <Routes />
+    </>
+  );
+};
+
+
